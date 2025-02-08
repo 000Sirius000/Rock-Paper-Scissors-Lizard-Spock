c7928685cc43050919485745f88aa494c8e5346f
Mykyta <dark.sirius999@gmail.com>
2025-02-08 17:36:03 +0200

    Add class RPSLSGame

--- Main.py
+++ Main.py
@@ -9,3 +9,18 @@ class Move(Enum):
     SCISSORS = 'scissors'
     LIZARD = 'lizard'
     SPOCK = 'spock'
+
+class RPSLSGame:
+    """
+    Клас для гри "Rock Paper Scissors Lizard Spock".
+    Принципи SOLID забезпечують чітке розділення відповідальностей,
+    DRY – уникнення дублювання коду, KISS – простоту реалізації.
+    """
+    # Правила перемоги: кожен хід перемагає перелічені ходи.
+    WINNING_MOVES = {
+        Move.ROCK: [Move.SCISSORS, Move.LIZARD],
+        Move.PAPER: [Move.ROCK, Move.SPOCK],
+        Move.SCISSORS: [Move.PAPER, Move.LIZARD],
+        Move.LIZARD: [Move.SPOCK, Move.PAPER],
+        Move.SPOCK: [Move.ROCK, Move.SCISSORS],
+    }
